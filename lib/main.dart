2cdf288c99a422d55ae85e0f1d0f485acf527ebe
David <davidpineda.co@gmail.com>
2021-01-26 17:50:04 +0000

    remove useless import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/basic.dart';
 
 void main() {
   runApp(QuizApp());
